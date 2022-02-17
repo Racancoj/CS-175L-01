26bb6ba83e4e1806ad6e9a0a98f5eb6a6104b2c4
Racancoj <91151475+Racancoj@users.noreply.github.com>
2022-02-17 11:49:02 -0500

    Add files via upload

--- restaraunt.py
+++ restaraunt.py
@@ -2,44 +2,59 @@
 # CS 175-03
 # Ez Racancoj
 
+x = 'yes'
 
-    
-vegetarian = False
-vegan = False
-gluten_free = False
-
-# User input
-response = input('Is anyone in your party a vegetarian ')
-
-if response == 'yes':
-    vegetarian = True
-         
-response = input('Is anyone in your party vegan ')
-
-if response == 'yes':
-    vegan = True
+while x == 'yes':
         
-response = input('Is anyone in you party gluten-free ')
+    vegetarian = False
+    vegan = False
+    gluten_free = False
 
-if response == 'yes':
-    gluten_free = True
+    # User input
+    response = input('Is anyone in your party a vegetarian ')
+
+    if response == 'yes':
+        vegetarian = True
+             
+    response = input('Is anyone in your party vegan ')
+
+    if response == 'yes':
+        vegan = True
+            
+    response = input('Is anyone in you party gluten-free ')
+
+    if response == 'yes':
+        gluten_free = True
+
+    print(
+        )
 
 
 
-# Which restaurants can your party go to
+    # Which restaurants can your party go to
 
-choices = 'Corner Café, ' + 'The Chef’s Kitchen, '
-    
-if gluten_free == False and vegan == False and vegetarian == False:
-    choices += 'Joe’s Gourmet Burgers, '
-    
-if vegan == False and gluten_free == False:
-    choices += 'Mama’s Fine Italian, ' + 'Main Street Pizza Company, '
+    choices = 'Corner Café, ' + 'The Chef’s Kitchen, '
         
+    if gluten_free == False and vegan == False and vegetarian == False:
+        choices += 'Joe’s Gourmet Burgers, '
+        
+    if vegan == False and gluten_free == False:
+        choices += 'Mama’s Fine Italian, ' + 'Main Street Pizza Company, '
+            
 
 
-print(f"Here are your restaurant choices: {choices}")
+    print(f"Here are your restaurant choices: {choices}")
 
-   
 
+    print(
+        )
+
+
+    v = input('If you want to do another search Enter (yes) if not Enter (no): ')
+
+    if v == 'no':
+        x = 'no'
+
+
+       
 
